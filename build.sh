e0e3b80d683df00879c505f384ea6e9bfcc7fc9d
Amir Nissim <amirn@everything.me>
2013-05-25 22:44:20 +0300

    analytics

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ cp -r _gh_pages/* .
 echo 'fixing links...'
 find . -type f \( -name *.html -o -name *.css -o -name *.js \) -exec sed -i "" 's/href="\//href="\/bootstrap\//g' {} \;
 
+echo 'replacing Google Analytics account'
+sed -i "" s/UA-146052-10/UA-38103508-1/ index.html
+
+
 echo 'done.'
