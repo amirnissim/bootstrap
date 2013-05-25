557b33e7e5daa8e98c31113e1d74e5f8b0aed1b5
Amir Nissim <amirn@everything.me>
2013-05-26 00:18:59 +0300

    update src links

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ git checkout 3.0.0-docs
 cp -r _gh_pages/* .
 echo 'fixing links...'
 find . -type f \( -name *.html -o -name *.css -o -name *.js \) -exec sed -i "" 's/href="\//href="\/bootstrap\//g' {} \;
+find . -type f \( -name *.html -o -name *.css -o -name *.js \) -exec sed -i "" 's/src="\//src="\/bootstrap\//g' {} \;
 
 echo 'replacing Google Analytics account'
 sed -i "" s/UA-146052-10/UA-38103508-1/ index.html
